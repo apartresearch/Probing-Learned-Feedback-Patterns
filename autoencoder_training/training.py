e9abaf7faecd31333f75a707c0ca241f9e9c6c36
Amirali <amirali@amiralis-mbp.lan>
2023-09-26 19:27:45 -0600

    check attention mask.

--- autoencoder_training/training.py
+++ autoencoder_training/training.py
@@ -102,11 +102,12 @@ def feature_representation(model, layer_name, input_data, hyperparameters, devic
     base_dataset = TensorDataset(base_activations_tensor)
     base_data_loader = DataLoader(base_dataset, batch_size=hyperparameters['batch_size'], shuffle=True)
     '''
+    print('\nBatch size is ' + hyperparameters['batch_size'] + '\n')
     base_data_loader = DataLoader(input_data, batch_size=hyperparameters['batch_size'], shuffle=True)
 
     # Get batch without popping
     first_batch = next(iter(base_data_loader))
-    print(f'First batch is {first_batch}')
+    print('\nFirst batch has attention mask:\n' + first_batch['attention_mask'] + f"\n\n Length: {len(first_batch['attention_mask'])}")
 
     first_activations_tensor = get_layer_activations(model, layer_name, first_batch, device).detach().clone().squeeze(1)
     input_size = first_activations_tensor.size(-1)
