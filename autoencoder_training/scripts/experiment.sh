d53f14fa9ddd0e428b9e4542e142718c27e489cf
YuanXi-NTU <2350572718@qq.com>
2023-10-02 10:00:04 +0100

    Add more to experiment bash script.

--- autoencoder_training/scripts/experiment.sh
+++ autoencoder_training/scripts/experiment.sh
@@ -1,1 +1,11 @@
-python3 experiment.py --base_model_name pythia-70m --reward_function utility_reward
+python3 experiment.py --base_model_name pythia-70m --reward_function sentiment_reward &
+sleep 120
+
+python3 experiment.py --base_model_name pythia-70m --reward_function utility_reward &
+sleep 120
+
+python3 experiment.py --base_model_name gpt-neo-125m --reward_function sentiment_reward &
+sleep 120
+
+python3 experiment.py --base_model_name gpt-neo-125m --reward_function utility_reward &
+sleep 120
