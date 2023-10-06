f2a0bb15789d662a3ba9b6f782a49187038c2b92
Amirali <amirali@amiralis-mbp.lan>
2023-10-06 02:08:09 -0600

    add more scripts.

--- autoencoder_training/scripts/experiment.sh
+++ autoencoder_training/scripts/experiment.sh
@@ -9,3 +9,6 @@ sleep 120
 
 python3 experiment.py --base_model_name gpt-neo-125m --reward_function utility_reward &
 sleep 120
+
+python3 experiment.py --base_model_name gpt-j-6b-sharded-bf16 --reward_function sentiment_reward &
+sleep 120
