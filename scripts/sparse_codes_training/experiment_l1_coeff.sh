e4290d07b4db194792ac4fb25738cbddaac96236
Amir Abdullah <amirabdullah19852020@gmail.com>
2023-11-19 03:32:09 -0700

    remove redundant arguments.

--- scripts/sparse_codes_training/experiment_l1_coeff.sh
+++ scripts/sparse_codes_training/experiment_l1_coeff.sh
@@ -1,33 +1,33 @@
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.0005 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.0005 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.001 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.001 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.002 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.002 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.003 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.003 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.004 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.004 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.005 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.005 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.006 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.006 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.007 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.007 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.008 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.008 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.009 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.009 &
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name l1_coeff_exploration --tied_weights True --num_epochs 1 --l1_coef 0.01 &
+--wandb_project_name l1_coeff_exploration --tied_weights --num_epochs 1 --l1_coef 0.01 &
 
