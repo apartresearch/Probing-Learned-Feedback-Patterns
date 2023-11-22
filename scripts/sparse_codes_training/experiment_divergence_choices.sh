af614d1be454e9cc7784912655d2f4cca4af4934
Amir Abdullah <amirabdullah19852020@gmail.com>
2023-11-22 06:35:41 -0700

    Standardize wandb name for divergences.

--- scripts/sparse_codes_training/experiment_divergence_choices.sh
+++ scripts/sparse_codes_training/experiment_divergence_choices.sh
@@ -1,9 +1,9 @@
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name divergence_choices_exploration --divergence_choice lowest_layers --tied_weights --num_epochs 1 &
+--wandb_project_name divergences_choices_exploration --divergence_choice lowest_layers --tied_weights --num_epochs 1 &
 sleep 120
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-70m --reward_function utility_reward \
---wandb_project_name divergence_choices_exploration --divergence_choice highest_divergence --num_epochs 1 &
+--wandb_project_name divergences_choices_exploration --divergence_choice highest_divergence --num_epochs 1 &
 sleep 120
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-160m --reward_function utility_reward \
@@ -11,5 +11,5 @@ python3 src/sparse_codes_training/experiment.py --base_model_name pythia-160m --
 sleep 120
 
 python3 src/sparse_codes_training/experiment.py --base_model_name pythia-160m --reward_function utility_reward \
---wandb_project_name divergences_choice_exploration --divergence_choice highest_divergence --tied_weights --num_epochs 1 &
+--wandb_project_name divergences_choices_exploration --divergence_choice highest_divergence --tied_weights --num_epochs 1 &
 sleep 120
