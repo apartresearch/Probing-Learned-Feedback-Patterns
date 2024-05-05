5f264f028539d7dc08876221859261e043501ea6
Amir Abdullah <amir@cynch.ai>
2024-05-05 12:53:43 -0600

    more cleanup.

--- reward_analyzer/rlhf_model_training/model_pipelines/unalignment_training_pipeline.py
+++
@@ -1,6 +0,0 @@
-from src.rlhf_model_training.model_pipelines.rlhf_model_pipeline import DPOModelPipeline
-
-class Unalignment_TrainingPipeline(DPOModelPipeline):
-    """
-    Extends DPOModelPipeline
-    """
